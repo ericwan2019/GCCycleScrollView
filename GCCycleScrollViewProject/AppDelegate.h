99e9a93438a0ef6cd4508f0bc52724bc5608ee3a
lajoin mac <lajoin-mac@lajoinmacdemacbook-pro.local>
2016-03-24 15:37:50 +0800

    Initial Commit

---
+++ GCCycleScrollViewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCCycleScrollViewProject
+//
+//  Created by 万鸿恩 on 16/3/24.
+//  Copyright © 2016年 万鸿恩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCCycleScrollViewProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCCycleScrollViewProject
+//
+//  Created by 万鸿恩 on 16/3/24.
+//  Copyright © 2016年 万鸿恩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
